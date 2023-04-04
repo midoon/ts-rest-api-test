cf00f8b83719251316d6f1c1fe5278e6c75aa312
midoon <harunkoding@gmail.com>
2023-04-05 05:26:46 +0700

    add pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, { Application } from 'express'
 import { routes } from './routes/index.'
+import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const port: Number = 4000
 
+// parse body request
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// cors handler
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    next()
+})
+
 routes(app)
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => logger.info(`Server is listening on port ${port}`))
--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import { logger } from '../utils/logger'
 
 export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Health Check Success')
   res.status(200).send({ data: 'Hello Health' })
 })
